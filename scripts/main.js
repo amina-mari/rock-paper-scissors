b10c06a600957b472c37247395ce7e6775b9e524
Mariana Amina <mariana.dantasweb7@gmail.com>
2021-03-08 10:49:35 -0300

    Make the game with alerts, prompts and confirms

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,63 @@
                 - Else, ask the user to enter a valid option and go back to line 3
         - Else, end the program.
 */
+
+
+let matchNumber = 0;
+let score = 0;
+
+function rockPaperScissors(){
+    alert("This is the Game Rock Papers Scissors");
+    if(confirm("Do you wanna play?")){
+        alert("Rules:\n \n- Rock beats Scissors \n- Paper beats Rock \n- Scissors beats paper");
+        alert("You will play against the computer")
+        let option = prompt("Chosse your option (no matter capitalization): \n- 1 or \"Rock\" to Rock \n- 2 or \"Paper\" to Paper \n- 3 or \"Scissors\" to Scissors").toLowerCase();
+        
+        if(option == "1" || option == "2" || option == "3" || option == "rock" || option == "paper" || option == "scissors"){
+
+            let computerOption = parseInt((Math.random() * 2).toFixed(0)) + 1;
+
+            console.log(computerOption)
+            if(option == "1" || option == "rock"){
+                if(computerOption == 2){
+                    alert("Oh no, you lose.")
+                }
+                else if(computerOption == 3){
+                    alert("Yay! You won!")
+                } 
+                else {
+                    alert("It's a draw")
+                }
+            }
+            else if(option == "2" || option == "paper"){
+                if(computerOption == 1){
+                    alert("Yay! You won!")
+                } 
+                else if(computerOption == 3){
+                    alert("Oh no, you lose.")
+                } 
+                else {
+                    alert("It's a draw")
+                }
+            } 
+            else {
+                if(computerOption == 1){
+                    alert("Oh no, you lose.")
+                } 
+                else if(computerOption == 2){
+                    alert("Yay! You won!")
+                } 
+                else{
+                    alert("It's  a draw")
+                }
+            }
+
+
+        } else {
+            alert("You need to enter a valid option.")
+        }
+
+    } else {
+        alert("No problem, have a good day!")
+    }
+}
