48b02dff09f8de2c42cdd4251dbbc0247e2167c3
Mariana Amina <mariana.dantasweb7@gmail.com>
2021-03-08 10:23:23 -0300

    Define HTML struture and Javascript pseudocode

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+/*  - Inform the user how to play the game 
+    - Ask if the user want to play
+        - If the user want to play
+            - Ask the user to enter rock, paper or scissors
+                - If the user enter a valid option
+                    - Add 1 to number of matches
+                    - Make a random choice to beat the user choice
+                    - Show the result
+                        - If the user wins, add 1 to the score
+                    - Ask if the user want to play again
+                        - If the user want, go back to line 2
+                        - If not, show score and end the program    
+                - Else, ask the user to enter a valid option and go back to line 3
+        - Else, end the program.
+*/
