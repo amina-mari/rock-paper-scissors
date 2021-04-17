b1f3c21e82f608197949acba51f0f7e19b254728
Mariana Amina <mariana.dantasweb7@gmail.com>
2021-04-17 17:30:56 -0300

    Show result in a div

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,18 @@
-/*  - Inform the user how to play the game 
+/*  
+    - Wait for button press
+        - If button is pressed
+            - Add 1 to number of matches
+            - Make a random choice to beat the user choice
+            - Show the result in a new div
+                - If the user wins, add 1 to the score
+                - If the user draws, add 1 to draws
+                - If the user loses, add 1 to loses
+            - Ask if the user want to play again
+                - If it is true, go back to line 2
+                 
+
+            
+- Inform the user how to play the game 
     - Ask the user to enter rock, paper or scissors
         - If the user enter a valid option
             - Add 1 to number of matches
@@ -12,109 +26,131 @@
 */
 
 
-function showRules() {
-    alert("Rules:\n \n- Rock beats Scissors \n- Paper beats Rock \n- Scissors beats Paper");
-    alert("You will play against the computer")
-}
-
-function checkValidOption(option) {
-    if (option != null || option != undefined) {
-        option = option.toLowerCase()
-        if (option == "1" || option == "2" || option == "3" || option == "rock" || option == "paper" || option == "scissors") {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    else {
-        return false;
-    }
-}
-
-function showComputerOption(computerOption) {
-    let arrayOptions = ["Rock", "Paper", "Scissors"];
-    alert("Computer choosed " + arrayOptions[computerOption - 1]);
-}
+// function showComputerOption(computerOption) {
+//     let arrayOptions = ["Rock", "Paper", "Scissors"];
+//     alert("Computer choosed " + arrayOptions[computerOption - 1]);
+// }
 
 function showWinLoseDraw(option, computerOption) {
-    if (option == "1" || option == "rock") {
+    
+    if (option == 1) {
         if (computerOption == 2) {
-            alert("Oh no, you lose.")
+            paraResult.textContent = 'Oh no, you lose';
+            //alert("Oh no, you lose.")
             computerScore++;
         }
         else if (computerOption == 3) {
-            alert("Yay! You won!")
+            paraResult.textContent = 'Yay! You won!';
+            //alert("Yay! You won!")
             score++;
         }
         else {
-            alert("It's a draw")
+            paraResult.textContent = 'It\'s a draw';
+            //alert("It's a draw")
             draws++;
         }
     }
-    else if (option == "2" || option == "paper") {
+    else if (option == 2) {
         if (computerOption == 1) {
-            alert("Yay! You won!")
+            paraResult.textContent = 'Yay! You won!';
+            //alert("Yay! You won!")
             score++;
         }
         else if (computerOption == 3) {
-            alert("Oh no, you lose.")
+            paraResult.textContent = 'Oh no, you lose';
+            //alert("Oh no, you lose.")
             computerScore++;
         }
         else {
-            alert("It's a draw")
+            paraResult.textContent = 'It\'s a draw';
+            //alert("It's a draw")
             draws++;
         }
     }
     else {
         if (computerOption == 1) {
-            alert("Oh no, you lose.")
+            paraResult.textContent = 'Oh no, you lose';
+            //alert("Oh no, you lose.")
             computerScore++;
         }
         else if (computerOption == 2) {
-            alert("Yay! You won!")
+            paraResult.textContent = 'Yay! You won!';
+            //alert("Yay! You won!")
             score++;
         }
         else {
-            alert("It's  a draw")
+            paraResult.textContent = 'It\'s a draw';
+            //alert("It's  a draw")
             draws++;
         }
     }
+    matchResult.appendChild(paraResult);
+    divGame.appendChild(matchResult);
 }
 
-function showScores() {
-    alert("You played " + matchNumber + " matches" + "\nYour final score was: " + score +
-        "\nThe final computer score was: " + computerScore + "\nYou had " + draws + " draws");
-    score = 0;
-    computerScore = 0;
-    matchNumber = 0;
-    draws = 0;
-}
+// function showScores() {
+//     alert("You played " + matchNumber + " matches" + "\nYour final score was: " + score +
+//         "\nThe final computer score was: " + computerScore + "\nYou had " + draws + " draws");
+//     score = 0;
+//     computerScore = 0;
+//     matchNumber = 0;
+//     draws = 0;
+// }
 
 let matchNumber = 0;
 let score = 0;
 let computerScore = 0;
 let draws = 0;
 
-function rockPaperScissors() {
-    alert("This is the Game Rock Papers Scissors");
-    showRules();
-    let option = prompt("Choose your option (no matter capitalization): \n- 1 or \"Rock\" to Rock \n- 2 or \"Paper\" to Paper \n- 3 or \"Scissors\" to Scissors");
+// Div to show match result
 
-    if (checkValidOption(option)) {
-        matchNumber++;
-        
-        let computerOption = parseInt((Math.random() * 2).toFixed(0)) + 1;
+let divGame = document.querySelector(".game");
+let matchResult = document.createElement("div");
+matchResult.classList.add("game__result");
 
-        showComputerOption(computerOption);
-        showWinLoseDraw(option, computerOption);
+let paraResult = document.createElement('p');
 
-        if (confirm("Wanna play again?")) {
-            rockPaperScissors();
-        } else {
-            showScores();
-        }
-    } else {
-        alert("You need to enter a valid option.")
+let closeButton = document.createElement('button');
+let closeImg = document.createElement("img")
+closeImg.setAttribute('src', 'images/close.png');
+closeButton.appendChild(closeImg);
+
+matchResult.appendChild(closeButton)
+
+// Event listener to close matchResult
+
+closeButton.addEventListener('click', function(event){
+    event.currentTarget.parentNode.classList.add('game__result--closed');
+})
+
+let buttons = document.querySelectorAll('.game__button')
+ 
+buttons.forEach(button => button.addEventListener('mousedown', rockPaperScissors, {capture:true}));
+
+function rockPaperScissors(event) {
+    matchNumber++;
+    let buttonPressed = event.currentTarget;
+    let option = buttonPressed.getAttribute('data-option');
+    let computerOption = parseInt((Math.random() * 2).toFixed(0)) + 1;
+    if(matchResult.classList.contains('game__result--closed')){
+        matchResult.classList.remove("game__result--closed")
     }
+    showWinLoseDraw(option, computerOption);
+
+
+    // if (checkValidOption(option)) {
+    //     matchNumber++;
+    
+
+    //     showComputerOption(computerOption);
+    //     showWinLoseDraw(option, computerOption);
+
+    //     if (confirm("Wanna play again?")) {
+        //         rockPaperScissors();
+    //     } else {
+    //         showScores();
+    //     }
+    // } else {
+    //     alert("You need to enter a valid option.")
+    // }
 }
